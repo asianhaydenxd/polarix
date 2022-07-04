097b38b50de4fa8539b686327c3022e4de510a7f
Hayden <asianhaydenxd@gmail.com>
2022-07-04 02:25:28 -0400

    Update lexer

--- lexer.py
+++ lexer.py
@@ -1,18 +1,271 @@
 from enum import Enum, auto
 from typing import List
+from typing_extensions import Self
+from unicodedata import category, lookup
 
-class Type(Enum):
-    SYM = auto() # Symbols (Identifiers, operators)
-    BRC = auto() # Brackets () [] {}
-    NUM = auto() # Numbers (23, 1.2)
-    CHR = auto() # Characters ('a')
-    STR = auto() # Strings ("Hello, world!")
-    EOF = auto() # End-of-file
+# Steps:
+#   Turn the string of code into tokens
+#   Organize the tokens into a tree
+#   Traverse tree to define functions and enum constructors
+#   Traverse and execute tree
+
+NUMBERS = "1234567890"
+WHITESPACE = "\n\v\t\r "
+
+RESERVED_CHARS = "()[]{},;\"\'"
+RESERVED_OPS = ["=", ":", "|", "->"]
+
+RESERVED_IDS = ["module", "import", "if", "otherwise", "enum", "struct", "mut", "infix"]
+
+class LexError:
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+class UnterminatedStringError(LexError):
+    def __init__(self, position):
+        super().__init__("UnterminatedStringError", position)
+
+class UnterminatedCharError(LexError):
+    def __init__(self, position):
+        super().__init__("UnterminatedCharError", position)
+
+class TerminatingNumericalDotError(LexError):
+    def __init__(self, position):
+        super().__init__("TerminatingNumericalDotError", position)
+
+class MultipleNumericalDotsError(LexError):
+    def __init__(self, position):
+        super().__init__("MultipleNumericalDotsError", position)
+
+class Position:
+    def __init__(self, index: int, line: int, x: int, file_name: str, file_text: str) -> None:
+        self.index = index
+        self.line = line
+        self.x = x
+        self.file_name = file_name
+        self.file_text = file_text
+
+    def next(self) -> None:
+        # Go to the next character in the text
+        self.index += 1
+        self.x += 1
+        if self.index < len(self.file_text):
+            if self.file_text[self.index] == '\n':
+                self.x = 0
+                self.line += 1
+
+    def in_range(self) -> bool:
+        return self.index < len(self.file_text)
+
+    def copy(self) -> Self:
+        return Position(self.index, self.line, self.x, self.file_name, self.file_text)
+
+    def __str__(self) -> str:
+        return f"line {self.line}, char {self.x}"
+
+class TokenCategory(Enum):
+    Symbol     = auto()
+    Identifier = auto()
+    String     = auto()
+    Character  = auto()
+    Number     = auto()
 
 class Token:
-    def __init__(self, type: Type, name: any = None) -> None:
-        self.type = type
+    def __init__(self, category: TokenCategory, name: any = None, start: Position = None, end: Position = None) -> None:
+        self.category = category
         self.name = name
 
-def lex(code: str) -> List[Token]:
-    return
+        if start:
+            self.start = start.copy()
+
+        if end:
+            self.end = end.copy()
+        elif start:
+            self.end = start.copy().next()
+    
+    def __str__(self) -> str:
+        match self.category:
+            case TokenCategory.Symbol: category = "sym"
+            case TokenCategory.Identifier: category = "id"
+            case TokenCategory.String: category = "str"
+            case TokenCategory.Character: category = "chr"
+            case TokenCategory.Number: category = "num"
+
+        return f"[{category}: {self.name}]"
+
+class Lexer():
+    def __init__(self, code: str):
+        self.code = code
+        self.tokens = []
+        self.pos = Position(0, 0, 0, "stdio", code)
+        self.lex()
+
+    def current_char(self) -> chr:
+        return self.code[self.pos.index]
+    
+    def lex(self) -> List[Token]:
+        while self.pos.index < len(self.code):
+            if self.current_char() in NUMBERS:
+                self.tokens.append(self.get_num())
+            elif self.current_char() in WHITESPACE:
+                self.pos.next()
+            elif self.current_char() == "\"":
+                self.tokens.append(self.get_string())
+            elif self.current_char() in RESERVED_CHARS:
+                self.tokens.append(Token(TokenCategory.Symbol, self.current_char(), self.pos))
+                self.pos.next()
+            else:
+                self.tokens.append(self.get_word())
+        return self.tokens
+
+    def get_num(self):
+        start_pos = self.pos.copy()
+        has_dot = False
+        last_dot = False
+
+        while self.pos.in_range() and self.current_char() in NUMBERS + ".":
+            if self.current_char() == ".":
+                if has_dot: raise MultipleNumericalDotsError(self.pos.copy())
+                has_dot = True
+                last_dot = True
+            else:
+                last_dot = False
+
+            self.pos.next()
+        
+        if last_dot: raise TerminatingNumericalDotError(self.pos.copy())
+
+        return Token(
+            TokenCategory.Number,
+            self.code[start_pos.index : self.pos.index],
+            start_pos,
+            self.pos
+        )
+
+    def get_string(self):
+        start_pos = self.pos.copy()
+
+        self.pos.next()
+
+        string = ""
+
+        while self.current_char() != "\"":
+            if self.current_char() == "\\":
+                string += self.next_escape()
+                continue
+            string += self.current_char()
+            self.pos.next()
+            if not self.pos.in_range() or self.current_char() == "\n":
+                raise UnterminatedStringError(start_pos)
+            
+            # implement string interpolation "Hello, ${name}!"
+        
+        self.pos.next()
+
+        return Token(
+            TokenCategory.String, 
+            string, 
+            start_pos, 
+            self.pos
+        )
+
+    def next_escape(self, delimiter="\""):
+        # String/char escaping
+        string = self.code
+        new_string = ""
+        self.pos.next()
+        if   self.current_char() == "a":  new_string += "\a"
+        elif self.current_char() == "b":  new_string += "\b"
+        elif self.current_char() == "e":  new_string += "\x1b"
+        elif self.current_char() == "f":  new_string += "\f"
+        elif self.current_char() == "n":  new_string += "\n"
+        elif self.current_char() == "r":  new_string += "\r"
+        elif self.current_char() == "s":  new_string += " "
+        elif self.current_char() == "t":  new_string += "\t"
+        elif self.current_char() == "v":  new_string += "\v"
+        elif self.current_char() == "$":  new_string += "$"
+        elif self.current_char() == "\\": new_string += "\\"
+        elif self.current_char() == "\'": new_string += "\'"
+        elif self.current_char() == "\"": new_string += "\""
+        elif self.current_char() == "N":
+            self.pos.next()
+            if self.current_char() != "{":
+                new_string += "\\N"
+            else:
+                self.pos.next()
+                lookup_code = ""
+                while self.current_char() != delimiter and self.current_char() != "}":
+                    lookup_code += self.current_char()
+                    self.pos.next()
+                if self.current_char() == "}":
+                    new_string += lookup(lookup_code)
+                else:
+                    new_string += "\\N" + lookup_code
+        elif self.current_char() == "x":
+            self.pos.next()
+            if self.current_char() not in "0123456789ABCDEFabcdef":
+                new_string += "\\x" + self.current_char()
+            else:
+                following_pos = self.pos.copy()
+                following_pos.next()
+                if self.code[following_pos.index] not in "0123456789ABCDEFabcdef":
+                    new_string += "\\x" + self.current_char()
+                else:
+                    new_string += bytearray.fromhex(self.current_char() + self.code[following_pos.index]).decode()
+                    self.pos.next()
+        elif self.current_char() == "u":
+            self.pos.next()
+            digits = ""
+            last_pos = None
+            while self.current_char() != delimiter and len(digits) < 4 and self.current_char() in "0123456789ABCDEFabcdef":
+                digits += self.current_char()
+                last_pos = self.pos.copy()
+                self.pos.next()
+            if len(digits) == 4:
+                new_string += chr(int(digits, 16))
+            else:
+                new_string += "\\u" + digits
+            self.pos = last_pos
+        elif self.current_char() == "U":
+            self.pos.next()
+            digits = ""
+            last_pos = None
+            while self.current_char() != delimiter and len(digits) < 8 and self.current_char() in "0123456789ABCDEFabcdef":
+                digits += self.current_char()
+                last_pos = self.pos.copy()
+                self.pos.next()
+            if len(digits) == 8:
+                new_string += chr(int(digits, 16))
+            else:
+                new_string += "\\U" + digits
+            self.pos = last_pos
+        elif self.current_char() in "01234567":
+            digits = ""
+            last_pos = None
+            while self.current_char() != delimiter and len(digits) < 3 and self.current_char() in "01234567":
+                digits += self.current_char()
+                last_pos = self.pos.copy()
+                self.pos.next()
+            new_string += chr(int(digits, 8))
+            self.pos = last_pos
+        else:
+            new_string += "\\"
+            new_string += self.current_char()
+        self.pos.next()
+
+        return new_string
+
+    def get_word(self):
+        unicode_category = category(self.current_char())
+        start_pos = self.pos.copy()
+        
+        while self.pos.in_range() and self.current_char() not in RESERVED_CHARS and category(self.current_char()).replace("N","L").replace("S", "P")[0] == unicode_category.replace("S", "P")[0]:
+            self.pos.next()
+
+        return Token(
+            TokenCategory.Symbol if self.code[start_pos.index : self.pos.index] in RESERVED_IDS + RESERVED_OPS else TokenCategory.Identifier, 
+            self.code[start_pos.index : self.pos.index], 
+            start_pos, 
+            self.pos
+        )
