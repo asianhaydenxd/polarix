2e82f2a8d1e612b44df3a192a0ffad01fdef0889
Hayden <asianhaydenxd@gmail.com>
2022-07-05 01:20:49 -0400

    Parse module header and imports

--- parser.py
+++ parser.py
@@ -14,12 +14,36 @@ class InvalidModuleNameError(ParseError):
     def __init__(self, token):
         super().__init__(token)
 
+class InvalidExportError(ParseError):
+    def __init__(self, token):
+        super().__init__(token)
+
+class InvalidImportError(ParseError):
+    def __init__(self, token):
+        super().__init__(token)
+
+class InvalidImportAliasError(ParseError):
+    def __init__(self, token):
+        super().__init__(token)
+
+class InvalidImportDeclarationError(ParseError):
+    def __init__(self, token):
+        super().__init__(token)
+
 # Nodes
 
 class ModuleNode:
-    def __init__(self, name, decls):
+    def __init__(self, name, exports=None, imports=None, decls=None):
         self.name = name
+        self.exports = exports
+        self.imports = imports
         self.decls = decls
+
+class ImportNode:
+    def __init__(self, module, alias=None, ids=None):
+        self.module = module
+        self.alias = alias if alias is not None else self.module
+        self.ids = ids
 
 class DeclarationNode:
     def __init__(self, name, node):
@@ -44,21 +68,120 @@ class Parser:
         self.index += 1
 
     def parse(self):
-        name, err = self.find_mod_header()
+        name, exports, err = self.find_mod_header()
+        if err is not None: return None, err
+
+        imports, err = self.find_imports()
+
+        module = ModuleNode(name, exports, imports, [])
+
+        # while self.current_token().category != TC.EndOfFile:
+        #     decl, err = self.get_decl()
+        #     self.advance()
+        
+        return module
     
     def find_mod_header(self):
         # Find module header token
         while self.current_token().tup not in [(TC.Symbol, "module"), (TC.EndOfFile, None)]:
             if self.current_token().tup not in [(TC.Symbol, "module"), (TC.NewLine, None)]:
-                return None, NoModuleHeaderError(self.current_token())
+                return None, None, NoModuleHeaderError(self.current_token())
             self.advance()
         
         if self.current_token().tup == (TC.EndOfFile, None):
-            return None, NoModuleHeaderError(self.current_token())
+            return None, None, NoModuleHeaderError(self.current_token())
         
         self.advance()
 
         if self.current_token().category != TC.Identifier:
-            return None, InvalidModuleNameError(self.current_token())
+            return None, None, InvalidModuleNameError(self.current_token())
         
-        return self.current_token().name, None
+        name = self.current_token().name
+
+        self.advance()
+
+        if self.current_token().tup != (TC.Symbol, "("):
+            return name, None, None
+        
+        exports = []
+
+        while self.current_token().tup != (TC.Symbol, ")"):
+            self.advance()
+
+            if self.current_token().category != TC.Identifier:
+                return None, None, InvalidExportError(self.current_token())
+            
+            exports.append(self.current_token().name)
+            self.advance()
+
+            if self.current_token().tup not in [(TC.Symbol, ","), (TC.Symbol, ")")]:
+                return None, None, InvalidExportError(self.current_token())
+        
+        self.advance()
+
+        return name, exports, None
+    
+    def find_imports(self):
+        imports = []
+
+        while self.current_token().tup not in [(TC.Symbol, "import"), (TC.EndOfFile, None)]:
+            if self.current_token().tup not in [(TC.Symbol, "import"), (TC.NewLine, None), (TC.Indent, None)]:
+                return [], None
+            self.advance()
+
+        while self.current_token().tup == (TC.Symbol, "import"):
+            self.advance()
+            if self.current_token().category != TC.Identifier:
+                return None, InvalidImportError(self.current_token())
+            
+            name = self.current_token().name
+            alias = None
+            import_ids = []
+
+            self.advance()
+
+            if self.current_token().tup == (TC.Symbol, "as"):
+                self.advance()
+                if self.current_token().category != TC.Identifier:
+                    return None, InvalidImportAliasError(self.current_token())
+                alias = self.current_token().name
+
+                self.advance()
+
+            if self.current_token().tup == (TC.Symbol, "("):
+                while self.current_token().tup != (TC.Symbol, ")"):
+                    self.advance()
+
+                    if self.current_token().category != TC.Identifier:
+                        return None, InvalidImportDeclarationError(self.current_token())
+                    
+                    import_ids.append(self.current_token().name)
+                    self.advance()
+
+                    if self.current_token().tup not in [(TC.Symbol, ","), (TC.Symbol, ")")]:
+                        return None, InvalidImportDeclarationError(self.current_token())
+                
+                self.advance()
+            
+            imports.append(ImportNode(name, alias, import_ids))
+        
+        return imports, None
+                    
+
+    # def get_decl(self):
+    #     if self.current_token().category == TC.Identifier:
+    #         pass
+    #     else:
+    #         pass
+
+if __name__ == "__main__":
+    import lexer
+    module = Parser(lexer.Lexer("""
+
+module Main
+
+import StdIO as IO
+
+    """).tokens).module
+
+    print(module.imports[0].ids)
