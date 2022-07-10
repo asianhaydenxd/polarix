569ed0c8321790d832a827fb27065343a808824f
Hayden <asianhaydenxd@gmail.com>
2022-07-10 18:01:38 -0400

    Add string lexing

--- Lexer.hs
+++ Lexer.hs
@@ -1,5 +1,11 @@
 module Lexer where
 
+data LexError
+    = UnclosedStringError
+    deriving Show
+
+data Handled a = Ok a | Error LexError deriving Show
+
 data Token
     = SymbolToken Symbol
     | IdentifierToken String
@@ -28,3 +34,19 @@ data Symbol
     | RightBracketSymbol
     | LeftCurlyBraceSymbol
     | RightCurlyBraceSymbol
+
+whitespace = " \n\v\t\f"
+
+lex :: String -> [Token]
+lex code = undefined
+
+-- Implement escape sequences
+-- Implement string interpolation
+lexStr :: String -> Handled String
+lexStr [] = Error UnclosedStringError
+lexStr ('"':cs) = Ok []
+lexStr (c:cs) = insertChar c (lexStr cs)
+
+insertChar :: Char -> Handled String -> Handled String
+insertChar c (Ok str) = Ok (c:str)
+insertChar c (Error err) = Error err
