bba2d1b9272e83f21ca54f571e689c1d48f81bd7
Hayden <asianhaydenxd@gmail.com>
2022-07-10 23:43:03 -0400

    Define types

--- Parser.hs
+++ Parser.hs
@@ -2,14 +2,18 @@ module Parser where
 
 import qualified Lexer
 
+data Type
+    = Type String
+    | Generic [Type]
+
+data Expression
+    = Literal Literal
+    | Function [Expression]
+    | BinOp Expression Expression
+
 data Literal
     = IdentifierLit String
     | IntegerLit String
     | FloatLit String
     | StringLit String
     | CharLit Char
-
-data Expression
-    = Literal Literal
-    | Function [Expression]
-    | BinOp Expression Expression
