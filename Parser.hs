628f1a7c665758df142bc0a892a9d9cb1567b2b6
Hayden <asianhaydenxd@gmail.com>
2022-07-10 23:37:58 -0400

    Define literals

--- Parser.hs
+++ Parser.hs
@@ -1,3 +1,10 @@
 module Parser where
 
 import qualified Lexer
+
+data Literal
+    = IdentifierLit String
+    | IntegerLit String
+    | FloatLit String
+    | StringLit String
+    | CharLit Char
