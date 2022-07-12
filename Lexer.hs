607470c7bdbb4a4e33df229b36b9542a769a67f1
Hayden <asianhaydenxd@gmail.com>
2022-07-12 01:23:38 -0400

    Add locations to tokens

--- Lexer.hs
+++ Lexer.hs
@@ -11,15 +11,35 @@ data Handled a
     | Error LexError
     deriving Show
 
+data Location = Location
+    { index :: Integer
+    , line :: Integer
+    , column :: Integer
+    , fileName :: String
+    , fileText :: String
+    }
+
+instance Show Location where
+    show (Location _ line column fileName _) = fileName ++ ":" ++ show line ++ ":" ++ show column
+
+startlocation :: String -> String -> Location
+startlocation name text = Location 0 0 0 name text
+
+next :: Location -> Location
+next (Location index line column fileName fileText)
+    | fromIntegral index >= length fileText    = Location (index + 1) line       (column + 1) fileName fileText
+    | fileText !! fromIntegral (index) == '\n' = Location (index + 1) (line + 1) 0            fileName fileText
+    | otherwise                                = Location (index + 1) line       (column + 1) fileName fileText
+
 data Token
-    = SymbolToken String
-    | IdentifierToken String
-    | OperatorToken String
-    | StringToken String
-    | CharToken Char
-    | NumberToken String
-    | NewLineToken
-    | EndOfFile
+    = SymbolToken     Location String
+    | IdentifierToken Location String
+    | OperatorToken   Location String
+    | StringToken     Location String
+    | CharToken       Location Char  
+    | NumberToken     Location String
+    | NewLineToken    Location
+    | EndOfFile       Location
     deriving Show
 
 data LexerState
@@ -44,46 +64,46 @@ infixr ->:
 (->:) x (Ok xs) = Ok (x:xs)
 (->:) x (Error err) = Error err
 
-tokenize :: String -> Handled [Token]
-tokenize code = lexer NoState (code ++ " ") [] where
-    lexer :: LexerState -> String -> String -> Handled [Token]
-    lexer NoState ('\"':cs) _ = lexer StringState cs []
-    lexer NoState ('\'':cs) _ = lexer CharState cs []
-    lexer NoState ('\n':cs) _ = NewLineToken ->: lexer NoState cs []
-    lexer NoState (c:cs) _ | c `elem` reserved_chars = SymbolToken [c] ->: lexer NoState cs []
-                           | c `elem` letters        = lexer WordState (c:cs) []
-                           | c `elem` symbols        = lexer OpState (c:cs) []
-                           | c `elem` numbers        = lexer NumState (c:cs) []
-                           | c `elem` whitespace     = lexer NoState cs []
+tokenize :: String -> String -> Handled [Token]
+tokenize name code = lexer NoState (startlocation name code) (code ++ " ") [] where
+    lexer :: LexerState -> Location -> String -> String -> Handled [Token]
+    lexer NoState l ('\"':cs) _ = lexer StringState (next l) cs []
+    lexer NoState l ('\'':cs) _ = lexer CharState (next l) cs []
+    lexer NoState l ('\n':cs) _ = NewLineToken l ->: lexer NoState (next l) cs []
+    lexer NoState l (c:cs) _ | c `elem` reserved_chars = SymbolToken l [c] ->: lexer NoState (next l) cs []
+                             | c `elem` letters        = lexer WordState l (c:cs) []
+                             | c `elem` symbols        = lexer OpState   l (c:cs) []
+                             | c `elem` numbers        = lexer NumState  l (c:cs) []
+                             | c `elem` whitespace     = lexer NoState   (next l) cs []
 
-    lexer WordState (c:cs) s | c `elem` (letters ++ numbers ++ word_symbols) = lexer WordState cs (s ++ [c])
-                             | otherwise = IdentifierToken s ->: lexer NoState (c:cs) []
+    lexer WordState l (c:cs) s | c `elem` (letters ++ numbers ++ word_symbols) = lexer WordState (next l) cs (s ++ [c])
+                               | otherwise = IdentifierToken l s ->: lexer NoState l (c:cs) []
 
-    lexer OpState (c:cs) s | c `elem` symbols && c `notElem` reserved_chars = lexer OpState cs (s ++ [c])
-                           | otherwise = OperatorToken s ->: lexer NoState (c:cs) []
+    lexer OpState l (c:cs) s | c `elem` symbols && c `notElem` reserved_chars = lexer OpState (next l) cs (s ++ [c])
+                             | otherwise = OperatorToken l s ->: lexer NoState l (c:cs) []
     
-    lexer NumState (c:cs) s | ('.' `notElem` s && c `elem` ('.':numbers)) || ('.' `elem` s && c `elem` numbers) = lexer NumState cs (s ++ [c])
-                            | otherwise = NumberToken s ->: lexer NoState (c:cs) []
+    lexer NumState l (c:cs) s | ('.' `notElem` s && c `elem` ('.':numbers)) || ('.' `elem` s && c `elem` numbers) = lexer NumState (next l) cs (s ++ [c])
+                              | otherwise = NumberToken l s ->: lexer NoState l (c:cs) []
     
     -- Implement string interpolation
-    lexer StringState [] _ = Error UnclosedStringError
-    lexer StringState ('\\':cs) s = escapeSeq StringState cs s
-    lexer StringState (c:cs) s | c /= '\"' = lexer StringState cs (s ++ [c])
-                               | otherwise = StringToken s ->: lexer NoState cs []
+    lexer StringState l [] _ = Error UnclosedStringError
+    lexer StringState l ('\\':cs) s = escapeSeq StringState (next l) cs s
+    lexer StringState l (c:cs) s | c /= '\"' = lexer StringState (next l) cs (s ++ [c])
+                                 | otherwise = StringToken l s ->: lexer NoState (next l) cs []
     
-    lexer _ [] _ = Ok [EndOfFile]
-    lexer _ _ _  = Error UnhandledStateError
+    lexer _ l [] _ = Ok [EndOfFile l]
+    lexer _ _ _ _  = Error UnhandledStateError
 
-    escapeSeq :: LexerState -> String -> String -> Handled [Token]
-    escapeSeq state ('a':cs) s = lexer state cs (s ++ "\a")
-    escapeSeq state ('b':cs) s = lexer state cs (s ++ "\b")
-    escapeSeq state ('f':cs) s = lexer state cs (s ++ "\f")
-    escapeSeq state ('n':cs) s = lexer state cs (s ++ "\n")
-    escapeSeq state ('r':cs) s = lexer state cs (s ++ "\r")
-    escapeSeq state ('s':cs) s = lexer state cs (s ++ " ")
-    escapeSeq state ('t':cs) s = lexer state cs (s ++ "\t")
-    escapeSeq state ('v':cs) s = lexer state cs (s ++ "\v")
-    escapeSeq state (c:cs)   s = Error UnrecognizedEscapeError
+    escapeSeq :: LexerState -> Location -> String -> String -> Handled [Token]
+    escapeSeq state l ('a':cs) s = lexer state (next l) cs (s ++ "\a")
+    escapeSeq state l ('b':cs) s = lexer state (next l) cs (s ++ "\b")
+    escapeSeq state l ('f':cs) s = lexer state (next l) cs (s ++ "\f")
+    escapeSeq state l ('n':cs) s = lexer state (next l) cs (s ++ "\n")
+    escapeSeq state l ('r':cs) s = lexer state (next l) cs (s ++ "\r")
+    escapeSeq state l ('s':cs) s = lexer state (next l) cs (s ++ " ")
+    escapeSeq state l ('t':cs) s = lexer state (next l) cs (s ++ "\t")
+    escapeSeq state l ('v':cs) s = lexer state (next l) cs (s ++ "\v")
+    escapeSeq state l (c:cs)   s = Error UnrecognizedEscapeError
 
 insertChar :: Char -> Handled String -> Handled String
 insertChar c (Ok str) = Ok (c:str)
